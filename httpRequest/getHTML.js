82c9ee29e7afda37c8cf153d0b8316e5945d1380
alfficcadenti <alfredoficcadenti@gmail.com>
2019-01-14 19:14:52 +0000

    function getHTML step 4 and linting previous steps files

--- httpRequest/getHTML.js
+++ httpRequest/getHTML.js
@@ -1,29 +1,32 @@
 var https = require('https');
 
+function getHTML (options, callback) {
 
+  https.get(options,callback, function () {
 
-function getAndPrintHTML (Options) {
-
-  https.get(Options, function (response) {
-
-    var chunks = "";
+    var html = "";
     // the callback is invoked when a `data` chunk is received
     response.on('data', function (data) {
-      chunks += data;
+      html += data;
     });
 
     // the callback is invoked when all of the data has been received
     // (the `end` of the stream)
-    response.on('end', function() {
-      console.log(chunks);
+    response.on('end', function(callback) {
+      callback(html);
       console.log('Response stream complete.');
     });
   });
+
+}
+
+function printHTML (html) {
+  console.log(html);
 }
 
 var requestOptions = {
   host: 'sytantris.github.io',
-  path: '/http-examples/step3.html'
+  path: '/http-examples/step4.html'
 };
 
-getAndPrintHTML(requestOptions);
+getHTML (requestOptions, printHTML);
